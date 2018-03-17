36f70e0288460e3d701c81b549a3e1fa791e4ad2
Katie West <katiewest820@gmail.com>
2018-03-16 17:33:11 -0700

    finished wiring up register

--- controllers/authController.js
+++ controllers/authController.js
@@ -31,6 +31,7 @@ exports.login = (req, res) => {
       userName: userSchema.userName,
     }
     token = jwt.sign(userToken, config.JWT_SECRET);
+    console.log(user)
     res.status(200).json({
       message: `${user.userName} successfully logged in`,
       userId: user._id,
@@ -73,10 +74,14 @@ exports.register = (req, res) => {
             message: 'Something bad happened'
           });
         }
-        res.status(200).send(newUser);
+        res.status(200).json({
+          message: 'New user created',
+          data: newUser
+        });
       });
     })
     .catch((err) => {
+      console.log(err)
       res.status(500).json({
         message: 'Something bad happened'
       });
